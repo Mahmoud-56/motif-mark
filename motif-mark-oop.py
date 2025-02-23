801b8128e2908147b0566d33c8ca36f16fabf49f
Mahmoud-56 <malm@uoregon.edu>
2025-02-22 16:07:36 -0800

    args

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python 
 
+import argparse as ap
+import cairo 
+import math 
+
+def get_args():
+    p = ap.ArgumentParser(description = "Indentify and count specefic motifs given a FASTA file < 1000 bp")
+    p.add_argument("-f", help = "FASTA file containing motids, exons are capitalized", required = True)
+    p.add_argument("-m", help = "Motif to search for in the FASTA file, one motif per line", required = True)
+    return p.parse_args()
+
 
