0173ab3802bca22d49ae5708d7b59d01a3e4f44d
Mahmoud-56 <malm@uoregon.edu>
2025-02-24 15:56:03 -0800

    iupac function

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -14,10 +14,18 @@ def get_args():
 class ParseSeq():
 
     def __init__(self, lines):
-
         self.gene = lines[0].split(" ")[0].strip(">")
         self.seq = "".join([line.strip('\n') for line in lines if not line.startswith(">")])
 
+    def seq_length(self):
+        return int(len(self.seq))
+
+    def iupac_name(motifs_files):
+
+        # Keys are nucleotides codes and values are possible bases
+
+        iupac = {"A":'[Aa]',"C":'[Cc]',"G":'[Gg]',"T":'[TUtu]',"U":'[TUtu]',"R":'[AGag]',"Y":'[CTct]',"S":'[GCgc]',"W":'[ATat]',"K":'[GTgt]',"M":'[ACac]',"B":'[CGTcgt]',"D":'[AGTagt]',"H":'[ACTact]',"V":'[ACGacg]',"N":'[A-Za-z]',
+                              "a":'[Aa]',"c":'[Cc]',"g":'[Gg]',"t":'[TUtu]',"u":'[TUtu]',"r":'[AGag]',"y":'[CTct]',"s":'[GCgc]',"w":'[ATat]',"k":'[GTgt]',"m":'[ACac]',"b":'[CGTcgt]',"d":'[AGTagt]',"h":'[ACTact]',"v":'[ACGacg]',"n":'[A-Za-z]'}
 
 
 
